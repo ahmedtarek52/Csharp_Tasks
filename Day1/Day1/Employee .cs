15523d7fc04c363b4a549a498d8122c9a3f9cb88
ahmedtarek52 <a7mdtarek52@gmail.com>
2024-12-18 18:08:49 +0200

    upload files

---
+++ Day1/Day1/Employee .cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    public enum Gender { Male, Female }
+
+    public struct Employee
+    {
+        public int id;
+        public float salary;
+        public HireDate hireDate;
+        public Gender gender;
+
+        public Employee()
+        {
+            id = 0;
+            salary = 0;
+            hireDate = new HireDate();
+            gender = Gender.Male;
+        }
+
+        public Employee(int _id, float _salary, HireDate _hireDate, Gender _gender)
+        {
+            id = _id;
+            salary = _salary;
+            hireDate = _hireDate;
+            gender = _gender;
+        }
+
+        
+        public int getID(){return id;}
+
+        public float getSalary() { return salary; }
+
+        public HireDate getHireDate() {return hireDate;}
+
+        public Gender getGender() {return gender;}
+
+
+        public void setId(int _id) {id = _id; }
+
+        public void setSalary(int _salary){ salary = _salary;}
+        public void setHireDate(HireDate _hireDate){ hireDate = _hireDate;}
+
+        public void setGender(Gender _gender) { gender = _gender; }
+    }
+
+    public struct HireDate
+    {
+        internal int day;
+        internal int month;
+        public int year;
+
+        public HireDate()
+        {
+            day = 0;
+            month = 0;
+            year = 0;
+        }
+
+        public int getDay(){return day; }
+
+        public int getMonth(){ return month;}
+        public int getYear(){ return year; }
+
+        public void setDay(int _day) { day = _day; }
+        public void setMonth(int _month) { month = _month; }
+        public void setYear(int _year) { year = _year; }
+    }
+}
---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Reflection;
+
+namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] Employees=new Employee[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Enter details of employee {i + 1}");
+
+                Console.Write("Enter employee ID: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter employee salary: ");
+                float salary = float.Parse(Console.ReadLine());
+
+                Console.Write("Enter employee gender (M/F): ");
+                Gender gender = Console.ReadLine()=="M" ? Gender.Male : Gender.Female;
+
+                Console.WriteLine("Enter employee hire date:");
+                Console.Write("Day: ");
+                int day = int.Parse(Console.ReadLine());
+                Console.Write("Month: ");
+                int month = int.Parse(Console.ReadLine());
+                Console.Write("Year: ");
+                int year = int.Parse(Console.ReadLine());
+
+                HireDate hireDate = new HireDate { day = day, month = month, year = year };
+
+                
+                Employees[i] = new Employee(id, salary, hireDate, gender);
+            }
+
+            Console.WriteLine("\nEmployee Details:\n");
+
+            for (int j = 0; j < Employees.Length; j++)
+            {
+                Console.WriteLine($"Employee {j + 1}:");
+                Console.WriteLine($"  ID: {Employees[j].id}");
+                Console.WriteLine($"  Salary: {Employees[j].salary}");
+                Console.WriteLine($"  Gender: {Employees[j].gender}");
+                Console.WriteLine($"  Hire Date: {Employees[j].hireDate.day}/{Employees[j].hireDate.month}/{Employees[j].hireDate.year}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
+    
+
